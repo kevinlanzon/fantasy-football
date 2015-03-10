4b9bc89d47bdfb045f07895c85c651ca089059ce
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-10 09:53:23 +0000

    cost added

--- js/football.js
+++ js/football.js
@@ -1,118 +1,33 @@
+var total_cost = 0
 for (var id = 1; id <= 40; id++) {
   $.ajax({
     url: "http://fantasy.premierleague.com/web/api/elements/" + id + "/",
     dataType: 'json'
     }).done(function(data) {
-      if(data.type_name === "Goalkeeper") {
-      $('.goalkeepers').append(
-        $('<option></option>').val(data.web_name, data.now_cost).html(data.web_name)
-        );
-      console.log(data)
-      player_cost = data.now_cost
-      console.log(player_cost)
-    }
-    else {
-      return false
-    }
+        var str = "." + data.type_name.toLowerCase() + "s"
+        $(str).append(
+          $('<option></option>').html(data.web_name).val(data.now_cost));
     });
 };
 
-for (var id = 1; id <= 40; id++) {
-  $.ajax({
-    url: "http://fantasy.premierleague.com/web/api/elements/" + id + "/",
-    dataType: 'json'
-    }).done(function(data) {
-      if(data.type_name === "Defender") {
-      $('.defenders').append(
-        $('<option></option>').val(data.web_name).html(data.web_name)
-        );
-    }
-    else {
-      return false
-    }
+function populateTeam(selectorClass, positionArray, positionClass, positionCode, positionMax){
+   $(selectorClass).change(function() {
+    var player = $('option:selected', $(this)).html();
+    var cost = $('option:selected', $(this)).val();
+
+    if(positionArray.indexOf(player) === -1 && positionArray.length < positionMax){
+      positionArray.push(player);
+      cost = parseInt(cost)
+      total_cost = total_cost + cost
+      $(positionClass).append(positionCode + positionArray[positionArray.length-1] + ' £' + cost/10 +'<br />');
+      $('.cost').text(total_cost/10 + 'm');
+      } else {return false}
     });
 };
 
-for (var id = 1; id <= 40; id++) {
-  $.ajax({
-    url: "http://fantasy.premierleague.com/web/api/elements/" + id + "/",
-    dataType: 'json'
-    }).done(function(data) {
-      if(data.type_name === "Midfielder") {
-      $('.midfielders').append(
-        $('<option></option>').val(data.web_name).html(data.web_name)
-        );
-    }
-    else {
-      return false
-    }
-    });
-};
+goalkeeperArray = []; defenderArray = []; midfielderArray = []; forwardArray = []
 
-for (var id = 1; id <= 40; id++) {
-  $.ajax({
-    url: "http://fantasy.premierleague.com/web/api/elements/" + id + "/",
-    dataType: 'json'
-    }).done(function(data) {
-      if(data.type_name === "Forward") {
-      $('.forwards').append(
-        $('<option></option>').val(data.web_name).html(data.web_name)
-        );
-    }
-    else {
-      return false
-    }
-    });
-};
-
-goalkeeperArray = []
-
- $('.goalkeepers').change(function() {
-    var player = $('option:selected', $(this)).val();
-    if(goalkeeperArray.indexOf(player) === -1 && goalkeeperArray.length < 2){
-    goalkeeperArray.push(player);
-  $('.gk').append('GK: ' + goalkeeperArray[goalkeeperArray.length-1] + '£' + player_cost + '<br />');
-  }
-  else {
-    return false
-  }
-  });
-
- defenderArray = []
-  $('.defenders').change(function() {
-    var player = $('option:selected', $(this)).text();
-    if(defenderArray.indexOf(player) === -1 && defenderArray.length < 5){
-    defenderArray.push(player);
-  $('.def').append('DEF: ' + defenderArray[defenderArray.length-1] + '<br />');
-  }
-  else {
-    return false
-  }
-  });
-
-
-  midfielderArray = []
-    $('.midfielders').change(function() {
-    var player = $('option:selected', $(this)).text();
-    if(midfielderArray.indexOf(player) === -1 && midfielderArray.length < 5){
-    midfielderArray.push(player);
-  $('.mid').append('MID: ' + midfielderArray[midfielderArray.length-1] + '<br />');
-  }
-  else {
-    return false
-  }
-  });
-
-  forwardArray = []
-    $('.forwards').change(function() {
-    var player = $('option:selected', $(this)).text();
-    if(forwardArray.indexOf(player) === -1 && forwardArray.length < 3){
-    forwardArray.push(player);
-  $('.fwd').append('FWD: ' + forwardArray[forwardArray.length-1] + '<br />');
-  }
-  else {
-    return false
-  }
-  });
-
-
+populateTeam('.goalkeepers', goalkeeperArray, '.gk', 'GK: ', 2)
+populateTeam('.defenders', defenderArray, '.def', 'DEF: ', 5)
+populateTeam('.midfielders', midfielderArray, '.mid', 'MID: ', 5)
+populateTeam('.forwards', forwardArray, '.fwd', 'FWD: ', 3)
